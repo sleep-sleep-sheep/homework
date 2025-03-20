0b749d71629f4aa953fb322f19b23ab51620d2ec
sleep-sleep-sheep <cld20051010@qq.com>
2025-03-20 09:55:28 +0800

    Add files via upload

---
+++ assignment4/second/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace _2025_3_20_assignment4
+{
+   public class AlarmClock
+    {
+        private int ClockTime;
+        public AlarmClock(int time)
+        {
+            ClockTime = time;
+        }
+        public event Action Tick;
+        public event Action Alarm;
+        public void Start()
+        {
+            for(int i=0;i<ClockTime;i++)
+            {
+                Tick();
+            }
+            Alarm();
+        }
+        
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock clock = new AlarmClock(10);
+            clock.Tick += () => Console.WriteLine("时钟滴答……");
+            clock.Alarm += () => Console.WriteLine("时钟闹铃……");
+            clock.Start();
+
+        }
+    }
+}
